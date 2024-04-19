e3eb7b3f7b2752701260cbad281388cd45f6f864
sunder32 <you@example.com>
2024-04-20 00:06:34 +0300

    fix: add more dump

--- bookshelf_tasks.sql
+++ bookshelf_tasks.sql
@@ -1,4 +1,5 @@
 -- 1
+
 SELECT authors.name, COUNT(authors_books.authors_id) AS book_count
 FROM authors
 JOIN authors_books ON authors.id = authors_books.authors_id
@@ -6,7 +7,9 @@ GROUP BY authors.name
 ORDER BY book_count DESC
 LIMIT 1;
 
+
 -- 2
+
 SELECT b.title, b.year 
 FROM books b
 WHERE b.year IS NOT NULL
@@ -15,6 +18,7 @@ LIMIT 5;
 
 
 -- 3
+
 SELECT COUNT(*)
 FROM books b
 JOIN shelves s ON b.shelves_id = s.id
@@ -30,12 +34,15 @@ JOIN authors a ON ab.authors_id = a.id
 JOIN shelves s ON b.shelves_id = s.id
 WHERE s.title ='Полка в спальне';
 
+
 -- 5
+
 SELECT b.title, b.year
 FROM books b
 JOIN authors_books ab ON b.id = ab.books_id
 JOIN authors a ON ab.authors_id = a.id
 WHERE a.name = 'Лев Толстой';
+
 
 -- 6
 
@@ -55,8 +62,8 @@ JOIN authors a ON ab.authors_id = a.id
 JOIN shelves s ON b.shelves_id = s.id
 WHERE s.title LIKE '%верхняя%' OR s.title LIKE '%нижняя%';
 
--- 8
 
+-- 8
 
 UPDATE books
 SET friends_id = (SELECT id FROM friends WHERE name = 'Иван Иванов')
@@ -66,21 +73,10 @@ WHERE title = 'Божественная комедия' AND authors_id = (SELECT
 -- 9
 
 INSERT INTO books (title, year, shelves_id)
-VALUES ('Краткие ответы на большие вопросы', 2020, (SELECT id FROM shelves WHERE title = 'кабинет'));
-
-INSERT INTO authors (name)
-SELECT 'Стивен Хокинг'
-WHERE NOT EXISTS (SELECT * FROM authors WHERE name = 'Стивен Хокинг');
-
-
-SET @author_id = (
-  SELECT id FROM authors WHERE name = 'Стивен Хокинг'
-);
-
+VALUES ('Краткие ответы на большие вопросы', 2020, (SELECT id FROM shelves WHERE title = 'полка в кабинете'));
 
 INSERT INTO authors_books (books_id, authors_id)
-VALUES ((SELECT id FROM books WHERE title = 'Краткие ответы на большие вопросы'), @author_id);
-
+VALUES ((SELECT id FROM books WHERE title = 'Краткие ответы на большие вопросы'), (SELECT id FROM authors WHERE name = 'Стивен Хокинг'));
 
 
 
